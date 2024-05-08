b894c792403d23b87cf0eeb79db857ce58047fbe
luki <lipka.luki@gmail.com>
2024-05-08 16:03:22 +0200

    Tracker

--- klienti/hlupy/player.py
+++ klienti/hlupy/player.py
@@ -1,8 +1,6 @@
 import random
 from probojPlayer import ProbojPlayer
 from constants import *
-from server.constants import Command
-
 
 class MyPlayer(ProbojPlayer):
     def make_turn(self) -> Command:
